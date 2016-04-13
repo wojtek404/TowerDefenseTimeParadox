dfd0cec13c00bad9395347d92fca57fcfe90aedc
wojtek404 <wojtek404@o2.pl>
2016-04-13 20:55:28 +0200

    Wstępny projekt.

---
+++ Assets/mini_orc_free/script/character.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Start () {
+GetComponent.<Animation>().Play("orcattack");
+yield WaitForSeconds(4.0);
+GetComponent.<Animation>().Play("orcwalk");
+yield WaitForSeconds(4.0);
+GetComponent.<Animation>().Play("orcdamage");
+yield WaitForSeconds(4.0);
+GetComponent.<Animation>().Play("orcdie");
+yield WaitForSeconds(4.0);
+}
