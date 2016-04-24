df52a2b5478c13ae909ae35cd04f526d25a73d26
skrzy <st.krzysiek6@gmail.com>
2016-04-24 19:11:21 +0200

    strzelająca wieża

---
+++ Assets/Scripts/Tower/RangeTrigger.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RangeTrigger : MonoBehaviour
+{
+    private TowerController towerScript;
+
+    void Start()
+    {
+        towerScript = transform.gameObject.GetComponent<TowerController>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject colGO = col.gameObject;
+
+        //skip it the object is not an enemy, or it died already
+        if (colGO.layer != SV.enemyLayer || !colGO.activeInHierarchy || PoolManager.Props[colGO.name].health <= 0)
+            return;
+
+        towerScript.inRange.Add(colGO);
+        //colGO.SendMessage("AddTower", towerScript);
+
+        //if an enemy has passed our range and we have an attackable target,
+        //start invoking TowerBase's CheckRange() method
+        //(StartInvoke() checks if it is running already, so it does not run twice)
+        if (towerScript.inRange.Count == 1)
+        {
+            towerScript.StartInvoke(0f);
+        }
+    }
+
+
+    //something has left our area of interest / collider range
+    void OnTriggerExit(Collider col)
+    {
+        //get collided object
+        GameObject colGO = col.gameObject;
+
+        //we don't need to check the enemy type again,
+        //we look up our inRange list instead and search this gameobject 
+        if (towerScript.inRange.Contains(colGO))
+        {
+            //collided object was added before and recognized as enemy
+            //enemy left our radius, remove from in range list
+            towerScript.inRange.Remove(colGO);
+            //and on the other side, remove our tower from enemy dictionary
+            //colGO.SendMessage("RemoveTower", towerScript);
+        }
+    }
+}
---
+++ Assets/Scripts/Tower/TowerController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TowerController : MonoBehaviour {
+
+    public GameObject projectile;
+
+    [HideInInspector]
+    public List<GameObject> inRange = new List<GameObject>();
+
+    [HideInInspector]
+    public Transform currentTarget;
+
+    public float projSpacing = 3;
+
+    public int damage;
+
+    void Start () {
+        StartInvoke(0f);
+    }
+
+    public void StartInvoke(float delay)
+    {
+        InvokeRepeating("CheckRange", delay + 0.1f, 1f);
+    }
+
+    void CheckRange()
+    {
+        if (inRange.Count == 0)
+        {
+            CancelInvoke("CheckRange");
+            currentTarget = null;
+            return;
+        }
+
+        AutoAttack();
+    }
+
+    void AutoAttack()
+    {
+        Transform target = GetTarget();
+        if (target == null)
+        {
+            CancelInvoke("CheckRange");
+            InvokeRepeating("CheckRange", 0.5f, 1f);
+            return;
+        }
+
+        InstantiateProjectile(target);
+    }
+
+    public Transform GetTarget()
+    {
+        if (inRange.Count <= 0) return null;
+        else return inRange[0].transform;
+    }
+
+    void InstantiateProjectile(Transform target)
+    {
+        GameObject projectileObj = (GameObject)Object.Instantiate(projectile, transform.position, new Quaternion());
+        projectileObj.SetActive(true);
+        Projectile proj = projectileObj.GetComponent<Projectile>();
+
+        proj.damage = damage;
+
+        proj.target = target;
+
+        
+    }
+}
