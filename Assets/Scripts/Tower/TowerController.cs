c15434a0d988fe36e7e0844d7012f9d61d1d4aa0
skrzy <st.krzysiek6@gmail.com>
2016-05-31 22:09:52 +0200

    flamethrower przy strzelaniu rakietami

--- Assets/Scripts/Tower/TowerController.cs
+++ Assets/Scripts/Tower/TowerController.cs
@@ -18,6 +18,8 @@ public class TowerController : MonoBehaviour {
     public Upgrade upgrade;
 
     public Transform projectileSpawnPoint;
+    public GameObject projectileFiringEffect;
+
 
     void Awake()
     {
@@ -81,6 +83,16 @@ public class TowerController : MonoBehaviour {
     {
         GameObject projectileObj = (GameObject)Object.Instantiate(projectile, projectileSpawnPoint.position, new Quaternion());
         projectileObj.SetActive(true);
+
+        if (projectileFiringEffect)
+        {
+            Quaternion firingEffectRotation = Quaternion.Euler(0, turret.rotation.eulerAngles.y + 90, 0);
+            Vector3 firingEffectPosition = projectileSpawnPoint.position;
+            firingEffectPosition.z += 2;
+            firingEffectPosition.y += 0.2f;
+            Object.Instantiate(projectileFiringEffect, firingEffectPosition, firingEffectRotation);
+        }
+
         Projectile proj = projectileObj.GetComponent<Projectile>();
         proj.damage = upgrade.options[upgrade.curLvl].damage;
         proj.target = target;
